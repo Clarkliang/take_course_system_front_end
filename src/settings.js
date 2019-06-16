e24ca1e3eb6f1e310d93a7fcc7c2592904e8fac2
Ljh <gzhuljh@163.com>
2019-06-17 03:24:29 +0800

    初始化项目

---
+++ src/settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: '学生选课系统',
+
+    /**
+     * @type {boolean} true | false
+     * @description Whether fix the header
+     */
+    fixedHeader: true,
+
+    /**
+     * @type {boolean} true | false
+     * @description Whether show the logo in sidebar
+     */
+    sidebarLogo: true,
+}
